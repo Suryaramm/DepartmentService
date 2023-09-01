30dd0c775a41f4837bceb67f72d7d9490adb435d
Surya5454 <pavansurya5454@gmail.com>
2023-09-01 12:39:47 +0000

    first commit

---
+++ src/main/java/com/springboot/departmentservice/service/DepartmentService.java
@@ -0,0 +1,16 @@
+package com.springboot.departmentservice.service;
+
+import com.springboot.departmentservice.dto.DepartmentDto;
+
+import java.util.List;
+
+public interface DepartmentService {
+
+    DepartmentDto createDepartment(DepartmentDto departmentDto);
+
+    List<DepartmentDto> getAllDepartments();
+    DepartmentDto getDepartmentById(Long id);
+    DepartmentDto updateDepartment(DepartmentDto departmentDto);
+
+    void deleteDepartment(Long id);
+}
